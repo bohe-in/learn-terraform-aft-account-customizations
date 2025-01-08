49f5012bac3ff461d64073d418b0caf5c59eae56
Moises Hermosillo <moises@Moisess-Mac-mini.local>
2025-01-08 11:37:03 -0600

    test update

--- bohecustom/terraform/s3.tf
+++ bohecustom/terraform/s3.tf
@@ -1,6 +1,9 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
+module "webapp-build-bucket" {
+  source = "./modules/base-modules/website-bucket"
+  bucket_name = "bohecustom"
+  allow_cors = true
+  error_document = "index.html"
+  index_document = "index.html"
 }
